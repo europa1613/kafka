b7042e28693c47754ec59b787bf4aaff222134d5
Arvind <europa1613@users.noreply.github.com>
2023-12-18 15:21:20 -0600

    ProducerConfig class

--- ud-bt-kafka/orderproducer/src/main/java/com/kafka/examples/OrderProducer.java
+++ ud-bt-kafka/orderproducer/src/main/java/com/kafka/examples/OrderProducer.java
@@ -1,16 +1,19 @@
 package com.kafka.examples;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
 
 public class OrderProducer {
     public static void main(String[] args) {
         Properties props = new Properties();
-        props.put("bootstrap.servers", "localhost:9092");
-        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        props.put("value.serializer", "org.apache.kafka.common.serialization.IntegerSerializer");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class.getName());
 
         try (KafkaProducer<String, Integer> producer = new KafkaProducer<>(props)) {
             ProducerRecord<String, Integer> record = new ProducerRecord<>("orders", "MacBookPro", 10);
