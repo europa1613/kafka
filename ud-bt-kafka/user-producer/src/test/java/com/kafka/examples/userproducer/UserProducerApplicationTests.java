ab9147021c43e8e7f57a3483f2c1a375a3b64c7b
Arvind <europa1613@users.noreply.github.com>
2023-12-19 16:23:13 -0600

    spring boot kafka: user-producer: initial

---
+++ ud-bt-kafka/user-producer/src/test/java/com/kafka/examples/userproducer/UserProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.examples.userproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
